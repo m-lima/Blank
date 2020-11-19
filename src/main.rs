6a4c3341292da85990e2f83a54bc99ad7391a80c
Marcelo Lima <marcelowind@gmail.com>
2020-11-19 12:46:46 +0100

    Adjust color temperature dynamically

--- src/main.rs
+++ src/main.rs
@@ -62,25 +62,41 @@ use crate::webview as runner;
 
 #[cfg(feature = "win")]
 mod winit {
+    use cocoa::appkit::{NSColor, NSWindow};
     use winit::{
         event::{ElementState, Event, KeyboardInput, ModifiersState, VirtualKeyCode, WindowEvent},
         event_loop::{ControlFlow, EventLoop},
         platform::macos::WindowExtMacOS,
-        window::{Fullscreen, WindowBuilder},
+        window::{Fullscreen, Window, WindowBuilder},
     };
 
-    pub fn run() {
-        let event_loop = EventLoop::new();
-
-        let color = unsafe {
-            cocoa::appkit::NSColor::colorWithRed_green_blue_alpha_(
-                cocoa::base::nil,
-                1.0,
-                0.8,
-                0.5,
+    fn ns_color_from_temperature(temperature: u32, old_id: cocoa::base::id) -> cocoa::base::id {
+        let (r, g, b) = temperagb::rgb_from_temperature(temperature).into();
+        unsafe {
+            NSColor::colorWithRed_green_blue_alpha_(
+                old_id,
+                f64::from(r) / 255.0,
+                f64::from(g) / 255.0,
+                f64::from(b) / 255.0,
                 1.0,
             )
-        };
+        }
+    }
+
+    fn set_background_color(window: &Window, color: cocoa::base::id) {
+        let ns_window = window.ns_window() as cocoa::base::id;
+        unsafe { ns_window.setBackgroundColor_(color) };
+    }
+
+    pub fn run() {
+        let mut current_modifiers = ModifiersState::default();
+        let mut released_w = true;
+        let mut released_q = true;
+        let mut graceful = false;
+        let mut temperature = 3000;
+        let mut color = ns_color_from_temperature(temperature, cocoa::base::nil);
+
+        let event_loop = EventLoop::new();
 
         let mut windows = event_loop
             .available_monitors()
@@ -90,18 +106,11 @@ mod winit {
                     .with_fullscreen(Some(Fullscreen::Borderless(Some(monitor))))
                     .build(&event_loop)
                     .unwrap();
-                use cocoa::appkit::NSWindow;
-                let ns_window = window.ns_window() as cocoa::base::id;
-                unsafe { ns_window.setBackgroundColor_(color) };
+                set_background_color(&window, color);
 
                 (window.id(), window)
             })
             .collect::<std::collections::HashMap<_, _>>();
-
-        let mut current_modifiers = ModifiersState::default();
-        let mut released_w = true;
-        let mut released_q = true;
-        let mut graceful = false;
 
         event_loop.run(move |event, _, control_flow| {
             if *control_flow == ControlFlow::Exit {
@@ -130,6 +139,20 @@ mod winit {
                     }
                     WindowEvent::ModifiersChanged(modifiers) => {
                         current_modifiers = modifiers;
+                    }
+                    WindowEvent::ReceivedCharacter('=') if temperature < 6600 => {
+                        temperature += 100;
+                        color = ns_color_from_temperature(temperature, color);
+                        windows
+                            .iter()
+                            .for_each(|window| set_background_color(window.1, color));
+                    }
+                    WindowEvent::ReceivedCharacter('-') if temperature > 1500 => {
+                        temperature -= 100;
+                        color = ns_color_from_temperature(temperature, color);
+                        windows
+                            .iter()
+                            .for_each(|window| set_background_color(window.1, color));
                     }
                     WindowEvent::KeyboardInput {
                         input:
